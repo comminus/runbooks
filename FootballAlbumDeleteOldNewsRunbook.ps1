16a855e69277fa3fc86db358a28a12b71efcadd6
comminus <39487289+comminus@users.noreply.github.com>
2018-05-24 14:42:15 +0200

    Update FootballAlbumDeleteOldNewsRunbook.ps1

--- FootballAlbumDeleteOldNewsRunbook.ps1
+++ FootballAlbumDeleteOldNewsRunbook.ps1
@@ -2,7 +2,6 @@
     .DESCRIPTION
         An example runbook which prints out the first 10 Azure VMs in your subscription (ordered alphabetically).
         For more information about how this runbook authenticates to your Azure subscription, see our documentation here: http://aka.ms/fxu3mn
-
     .NOTES
         AUTHOR: Azure Automation Team
         LASTEDIT: Mar 27, 2015
